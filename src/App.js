b92c42cde92a00f2ea63ed818bd4ffc07af93d92
joye <shonubij@gmail.com>
2019-12-27 08:54:47 +0100

    created error function component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from "react";
 import "./App.css";
 import Toggle from "./Toggle/Toggle";
+import Fetch from "./Fetch/Fetch";
 
+function ErrorMessage({ error }) {
+  return <div> {error} </div>;
+}
+
+function Ricky({ data }) {
+  return <div> There is data available</div>;
+}
 function App() {
   const handleToggle = () => {
     console.log("Toggled");
   };
   return (
-    <div>
-      <Toggle onToggle={handleToggle}>
-        <Toggle.Button></Toggle.Button>
-        <Toggle.On>The button is on</Toggle.On>
-        <Toggle.Off>The button is off</Toggle.Off>
+    // <div>
+    //   <Toggle onToggle={handleToggle}>
+    //     <Toggle.Button></Toggle.Button>
+    //     <Toggle.On>The button is on</Toggle.On>
+    //     <Toggle.Off>The button is off</Toggle.Off>
 
-        <span>Wonderful</span>
-      </Toggle>
-    </div>
+    //     <span>Wonderful</span>
+    //   </Toggle>
+    // </div>
+
+    <Fetch
+      loading={"Loading"}
+      error={error => <ErrorMessage error={error} />}
+      render={data => <Ricky data={data} />}
+    />
   );
 }
 
--- src/Fetch/Fetch.js
+++ src/Fetch/Fetch.js
@@ -4,22 +4,28 @@ import PropTypes from "prop-types";
 class Fetch extends React.Component {
   state = {
     data: "",
-    error: ""
+    error: "",
+    loading: false
   };
 
+  componentDidMount() {
+    this.setState({ loading: true }, () => this.fetchData());
+  }
   async fetchData() {
     try {
       let result = await fetch(this.props.url);
       let data = await result.json();
       this.setState({ data, loading: false });
     } catch (error) {
-      this.setState({ error });
+      this.setState({ error, loading: false });
     }
   }
   render() {
+    const { data, loading, error } = this.state;
     if (loading) return this.props.loading;
-    if (error) return this.props.error;
-    return this.props.render;
+    if (error) return this.props.error(error);
+    if (data) return this.props.render(data);
+    return null;
   }
 }
 
